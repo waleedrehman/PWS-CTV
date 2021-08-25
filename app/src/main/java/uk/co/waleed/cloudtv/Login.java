ed9b59109b0140cc067982df386c3cfa1b775235
Muhammad Waleed ur Rehman <waleedrehman@yahoo.co.uk>
2021-08-25 15:58:08 +0100

    Changes to login user and pass field to make it more attractive; added a toast for login to get the detailz

--- app/src/main/java/uk/co/waleed/cloudtv/Login.java
+++ app/src/main/java/uk/co/waleed/cloudtv/Login.java
@@ -6,14 +6,34 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class Login extends Activity {
+
+    EditText username, pass;
+    Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        username = (EditText) findViewById(R.id.username_input);
+        pass = (EditText) findViewById(R.id.pass);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password;
+                email = username.getText().toString();
+                password = pass.getText().toString();
+                Toast.makeText(Login.this, "Username: " + email +  "\nPassword: " + password, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
